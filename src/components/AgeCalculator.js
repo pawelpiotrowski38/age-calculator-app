64e79120eee708a9c3b9dc7cc76170b0eb8222af
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-09-19 19:30:17 +0200

    Style AgeCalculator component (mobile version)

--- src/components/AgeCalculator.js
+++ src/components/AgeCalculator.js
@@ -3,29 +3,49 @@ import '../styles.css';
 export default function AgeCalculator() {
     return (
         <section className="age-calculator">
-            <div className='age-calculator__form'>
+            <form className='age-calculator__form'>
                 <div className='age-calculator__form-items'>
                     <div className='age-calculator__form-item'>
-                        <label className='age-calculator__form-label'>
+                        <label className='age-calculator__form-label' htmlFor='day'>
                             DAY
                         </label>
-                        <input className='age-calculator__form-input' />
+                        <input
+                            className='age-calculator__form-input'
+                            id='day'
+                            name='day'
+                            placeholder='DD'
+                        />
                     </div>
                     <div className='age-calculator__form-item'>
-                        <label className='age-calculator__form-label'>
+                        <label className='age-calculator__form-label' htmlFor='month'>
                             MONTH
                         </label>
-                        <input className='age-calculator__form-input' />
+                        <input
+                            className='age-calculator__form-input'
+                            id='month'
+                            name='month'
+                            placeholder='MM'
+                        />
                     </div>
                     <div className='age-calculator__form-item'>
-                        <label className='age-calculator__form-label'>
+                        <label className='age-calculator__form-label' htmlFor='year'>
                             YEAR
                         </label>
-                        <input className='age-calculator__form-input' />
+                        <input
+                            className='age-calculator__form-input'
+                            id='year'
+                            name='year'
+                            placeholder='YYYY'
+                        />
                     </div>
                 </div>
-                <button className='button'></button>
-            </div>
+                <div className='age-calculator__button-container'>
+                    <span className='age-calculator__button-container-span'></span>
+                    <button className='button'>
+                        <img src='/images/icon-arrow.svg' alt="arrow" />
+                    </button>
+                </div>
+            </form>
             <div className='age-calculator__results'>
                 <p className='age-calculator__result'>
                     <span>38</span> years
