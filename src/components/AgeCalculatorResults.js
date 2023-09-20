a1f1a18e26228d1b73342d103063eda57247720d
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-09-20 18:31:54 +0200

    Create separate AgeCalculatorResultsItem component

--- src/components/AgeCalculatorResults.js
+++ src/components/AgeCalculatorResults.js
@@ -1,75 +1,12 @@
-import { useEffect, useState } from "react";
+import AgeCalculatorResultsItem from "./AgeCalculatorResultsItem";
 import '../styles/AgeCalculatorResults.css';
 
 export default function AgeCalculatorResults({ isResult, result }) {
-    const [currentYearValue, setCurrentYearValue] = useState(0);
-    const [currentMonthValue, setCurrentMonthValue] = useState(0);
-    const [currentDayValue, setCurrentDayValue] = useState(0);
-    const animationTime = 500;
-
-    useEffect(() => {
-        let startTimestamp;
-        
-        if (isResult) {
-            const animate = (timestamp) => {
-                if (!startTimestamp) {
-                    startTimestamp = timestamp
-                }
-                const elapsedTime = timestamp - startTimestamp;
-            
-                if (elapsedTime < animationTime) {
-                    const progress = elapsedTime / animationTime;
-                    setCurrentYearValue((result.years * progress).toFixed(0));
-                    setCurrentMonthValue((result.months * progress).toFixed(0));
-                    setCurrentDayValue((result.days * progress).toFixed(0));
-
-                    requestAnimationFrame(animate);
-                } else {
-                    setCurrentYearValue(result.years);
-                    setCurrentMonthValue(result.months);
-                    setCurrentDayValue(result.days);
-                }
-            };
-        
-            requestAnimationFrame(animate);
-        }
-    }, [result, isResult]);
-
     return (
         <div className='age-calculator-results'>
-            <div className='age-calculator-results__result'>
-                {isResult ? (
-                    <span className="age-calculator-results__result--value">{currentYearValue}</span>
-                ) : (
-                    <div className="age-calculator-results__result--none">
-                        <span>-</span>
-                        <span>-</span>
-                    </div>
-                )}
-                {result.years === 1 ? 'year' : 'years'}
-            </div>
-            <div className='age-calculator-results__result'>
-                {isResult ? (
-                    <span className="age-calculator-results__result--value">{currentMonthValue}</span>
-                ) : (
-                    <div className="age-calculator-results__result--none">
-                        <span>-</span>
-                        <span>-</span>
-                    </div>
-                )}
-                {result.months === 1 ? 'month' : 'months'}
-            </div>
-            <div className='age-calculator-results__result'>
-                {isResult ? (
-                    <span className="age-calculator-results__result--value">{currentDayValue}</span>
-                ) : (
-                    <div className="age-calculator-results__result--none">
-                        <span>-</span>
-                        <span>-</span>
-                    </div>
-                )}
-                {result.days === 1 ? 'day' : 'days'}
-            </div>
+            <AgeCalculatorResultsItem isResult={isResult} result={result.years} text='year' />
+            <AgeCalculatorResultsItem isResult={isResult} result={result.months} text='month' />
+            <AgeCalculatorResultsItem isResult={isResult} result={result.days} text='day' />
         </div>
     )
 }
---
+++ src/components/AgeCalculatorResultsItem.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import '../styles/AgeCalculatorResultsItem.css';
+
+export default function AgeCalculatorResultsItem({ isResult, result, text }) {
+    const [currentValue, setCurrentValue] = useState(0);
+    const animationTime = 500;
+
+    useEffect(() => {
+        let startTimestamp;
+        
+        if (isResult) {
+            const animate = (timestamp) => {
+                if (!startTimestamp) {
+                    startTimestamp = timestamp
+                }
+                const elapsedTime = timestamp - startTimestamp;
+            
+                if (elapsedTime < animationTime) {
+                    const progress = elapsedTime / animationTime;
+                    setCurrentValue((result * progress).toFixed(0));
+                    requestAnimationFrame(animate);
+                } else {
+                    setCurrentValue(result);
+                }
+            };
+        
+            requestAnimationFrame(animate);
+        }
+    }, [result, isResult]);
+
+    return (
+        <div className='age-calculator-results-item'>
+            {isResult ? (
+                <span className="age-calculator-results-item__value">{currentValue}</span>
+            ) : (
+                <div className="age-calculator-results-item__none">
+                    <span>-</span>
+                    <span>-</span>
+                </div>
+            )}
+            {result === 1 ? `${text}` : `${text}s`}
+        </div>
+    )
+}
